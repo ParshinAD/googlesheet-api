286e8c724d7783acdfd65622d6e22869aec9f1cc
ParshinAD <48247414+ParshinAD@users.noreply.github.com>
2022-09-09 15:57:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,5 @@
 # database config
 
-# set True if you use PostgreSQL on your local machine, or false if you use PostgreSQL on docker with Web UI
-local_db = True
-docker_web_db = not local_db
-
-if local_db:
-    host = '127.0.0.1'
-elif docker_web_db:
-    host = '192.168.99.100' # ip my virtual box ip.  Maybe you can use '0.0.0.0'
-
-
 user = 'user'
 password = 'password'
 db_name = 'db_name'
@@ -21,4 +11,15 @@ api_id = 'api_id'
 api_hash = 'api_hash'
 token = 'token'
 phone = 'phone'
-user_id = '196953153'
+user_id = 'user_id'
+
+# set True if you use PostgreSQL on your local machine, or false if you use PostgreSQL on docker with Web UI
+# better use local_db = True
+local_db = True
+docker_web_db = not local_db
+
+if local_db:
+    host = '127.0.0.1'
+elif docker_web_db:
+    host = '192.168.99.100' # ip my virtual box ip.  Maybe you can use '0.0.0.0'
+
